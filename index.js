303a8f05509f4e48ea5380fb13a66c9f6357199e
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-10-26 03:02:20 +0600

    single data has been found by using find

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const courseCategories = require("./data/course.json");
+const courseDetails = require("./data/course-details.json");
+const { request, application } = require("express");
 
 app.get("/", (req, res) => {
-    res.send("course API is running");
+  res.send("course API is running");
+});
+
+app.get("/course-categories", (req, res) => {
+  res.send(courseCategories);
+});
+
+
+app.get("/course/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courseDetails.find(selectedCourse => selectedCourse._id === id)
+    res.send(selectedCourses);
 })
 
-app.listen(port, () =>{
-    console.log("course API is running on", port);
-})
+app.listen(port, () => {
+  console.log("course API is running on", port);
+});
