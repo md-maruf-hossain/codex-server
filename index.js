cd8a4d5367f8afb05870106da66a8dc5b7017ddf
marufhossain6470 <104669410+marufhossain6470@users.noreply.github.com>
2022-10-26 23:30:21 +0600

    git ignore added

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
 const courseCategories = require("./data/course.json");
 const courseDetails = require("./data/course-details.json");
-const { request, application } = require("express");
 
 app.get("/", (req, res) => {
   res.send("course API is running");
@@ -17,12 +16,21 @@ app.get("/course-categories", (req, res) => {
   res.send(courseCategories);
 });
 
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "10") {
+    res.send(courseDetails);
+  } else {
+    const categoryCourseDetails = courseDetails.filter((courseDetail) => courseDetail.category_id === id);
+    res.send(categoryCourseDetails);
+  }
+});
 
 app.get("/course/:id", (req, res) => {
-    const id = req.params.id;
-    const selectedCourses = courseDetails.find(selectedCourse => selectedCourse._id === id)
-    res.send(selectedCourses);
-})
+  const id = req.params.id;
+  const selectedCourses = courseDetails.find((selectedCourse) => selectedCourse._id === id);
+  res.send(selectedCourses);
+});
 
 app.listen(port, () => {
   console.log("course API is running on", port);
